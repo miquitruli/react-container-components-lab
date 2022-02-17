405945fff8a05c6435cd98092edfad2a7808fa0d
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-16 21:20:22 -0500

    commit tree 4f8477cfa93ce107f2f48c477ac2107d58259c9e

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
 // Code MovieReviews Here
+import React from "react";
+
+const Review = ({ movieTitle, review }) => {
+    return(
+        <div className="review">
+            <h1>{movieTitle}</h1>
+            <p>{review}</p>
+        </div>
+    )
+}
+
+const MovieReviews = ({ reviews }) => {
+    return (
+      <div className="review-list">
+            {reviews.map(Review)}
+      </div>
+    )
+  }
+
+  MovieReviews.defaultProps = {
+    reviews: []
+}
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,50 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'uA5ms9h4Sbq3MiAArOB5mBkaKcfcl9VY';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    state = {
+        reviews: [],
+        searchTerm: ""
+      }
+    
+      renderReviews = () => {
+        if (this.state.reviews.length > 0) {
+          return <MovieReviews reviews={this.state.reviews}/>
+        }
+      }
+    
+      textFieldChange = (event) => this.setState({searchTerm: event.target.value})
+    
+      handleSubmit = (event) => {
+        event.preventDefault()
+        fetch(URL + `api-key=${NYT_API_KEY}`)
+          .then(resp => resp.json())
+          .then(json => {
+            this.setState({
+              reviews: json.results
+            })
+          })
+      }
+    
+      render(){
+        return (
+          <div className="searchable-movie-reviews">
+            <form onSubmit={event => this.handleSubmit(event)}>
+              <input
+                type="text"
+                value={this.state.searchTerm}
+                onChange={(event) => this.textFieldChange(event)}
+              />
+              <input type="submit"/>
+            </form>
+            {this.renderReviews()}
+          </div>
+        )
+      }
+    }
+export default SearchableMovieReviewsContainer
